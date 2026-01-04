503917f10c6cfe5674850985faa94355b1d81aad
rurikvlad-alt <rurikvlad@gmail.com>
2026-01-04 02:17:18 -0500

    Fix indentation in populate_indicators function

--- RSIBB_Momentum_Pro.py
+++ RSIBB_Momentum_Pro.py
@@ -1,34 +1,30 @@
-from freqtrade.strategy import IStrategy, IntParameter, DecimalParameter
-from pandas import DataFrame
-import technical.indicators as qtpylib
+import pandas as pd
 import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+from freqtrade.strategy.interface import IStrategy
+from pandas import DataFrame
 
 class RSIBB_Momentum_Pro(IStrategy):
-    timeframe = "1h"
-    startup_candle_count = 30
-    process_only_new_candles = True
-    use_exit_signal = True
-    exit_profit_only = False
-    ignore_roi_if_entry_signal = False
-
-    minimal_roi = {"0": 0.02, "60": 0.015, "180": 0.01, "360": 0}
+    timeframe = '1h'
+    minimal_roi = {
+        "0": 0.02,
+        "60": 0.015,
+        "180": 0.01,
+        "360": 0
+    }
     stoploss = -0.03
     trailing_stop = True
     trailing_stop_positive = 0.01
     trailing_stop_positive_offset = 0.015
     trailing_only_offset_is_reached = True
 
-    rsi_buy = IntParameter(20, 40, default=30, space="buy")
-    rsi_sell = IntParameter(60, 80, default=70, space="sell")
-    bb_width_min = DecimalParameter(0.010, 0.050, default=0.02, space="buy")
-
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-    boll = qtpylib.bollinger_bands(dataframe["close"], period=20, stddev=2)
-    dataframe["bb_upperband"] = boll["upper"]
-    dataframe["bb_middleband"] = boll["mid"]
-    dataframe["bb_lowerband"] = boll["lower"]
-    dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
-    return dataframe
+        boll = qtpylib.bollinger_bands(dataframe["close"], period=20, stddev=2)
+        dataframe["bb_upperband"] = boll["upper"]
+        dataframe["bb_middleband"] = boll["mid"]
+        dataframe["bb_lowerband"] = boll["lower"]
+        dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
+        return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
